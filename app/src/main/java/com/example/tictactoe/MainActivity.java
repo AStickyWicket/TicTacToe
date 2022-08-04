b863e62c641687d23db18b537affa5feffcddd15
durnf <dreadlordfluffybunny@gmail.com>
2022-08-04 11:09:55 -0230

    Fun with TicTacToe

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -1,9 +1,95 @@
 package com.example.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    boolean disableBoard = false;
+    //0 = X 1 = O
+    int currentPlayer = 0;
+    int[] currentBoard = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winConditions = {
+            {0, 1, 2},
+            {3, 4, 5},
+            {6, 7, 8},
+            {0, 3, 6},
+            {1, 4, 7},
+            {2, 5, 8},
+            {0, 4, 8},
+            {2, 4, 6}
+    };
+
+    int turnCounter = 0;
+
+    public void resetBoard(View view) {
+        disableBoard = false;
+        currentPlayer = 0;
+        turnCounter = 0;
+        Arrays.fill(currentBoard, 2);
+
+        findViewById(R.id.btnPlayAgain).setVisibility(View.INVISIBLE);
+        ((ImageView) findViewById(R.id.imgvTopLeft)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvTopMiddle)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvTopRight)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvMiddleLeft)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvMiddle)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvMiddleRight)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvBottomLeft)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvBottomMiddle)).setImageResource(0);
+        ((ImageView) findViewById(R.id.imgvBottomRight)).setImageResource(0);
+    }
+
+    public void onPlayerClick(View view) {
+        ImageView img = (ImageView) view;
+        int selectedImg = Integer.parseInt(img.getTag().toString());
+        Button playAgain = findViewById(R.id.btnPlayAgain);
+
+        if (!disableBoard) {
+
+            if (currentBoard[selectedImg] == 2) {
+                currentBoard[selectedImg] = currentPlayer;
+                turnCounter++;
+
+                if (currentPlayer == 0) {
+                    img.setImageResource(R.drawable.img_x);
+                    currentPlayer = 1;
+                } else {
+                    img.setImageResource(R.drawable.img_o);
+                    currentPlayer = 0;
+                }
+
+                boolean gameWin = false;
+                //Check for winner
+                for (int[] winCondition : winConditions) {
+                    if (currentBoard[winCondition[0]] == currentBoard[winCondition[1]] &&
+                            currentBoard[winCondition[1]] == currentBoard[winCondition[2]]
+                            && currentBoard[winCondition[0]] != 2) {
+                        gameWin = true;
+                        playAgain.setVisibility(View.VISIBLE);
+                        disableBoard = true;
+
+                        if (currentBoard[winCondition[0]] == 0) {
+                            Toast.makeText(this, "X wins!", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(this, "O wins!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+                if (turnCounter == 9 && !gameWin) {
+                    Toast.makeText(this, "Draw!", Toast.LENGTH_LONG).show();
+                    playAgain.setVisibility(View.VISIBLE);
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
